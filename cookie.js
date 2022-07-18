5b93d7e0fd7a412d5bed4f985e738e35205f1cd5
Yuktha20 <82943243+Yuktha20@users.noreply.github.com>
2022-07-18 13:59:26 +0530

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,5 +1,5 @@
 function setcookie() {
-  document.cookie = "Email = abc@gmail.com; expires=Thu, 18 Dec 2022 12:00:00 UTC";";
+  document.cookie = "Email = abc@gmail.com; expires=Thu, 18 Dec 2022 12:00:00 UTC";
 }
 function getcookie() {
   let x = document.cookie;
